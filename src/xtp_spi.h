233196e190ae02931032495b4af25bce2e32f81d
Jože Vovk <jozo132@gmail.com>
2025-07-09 22:54:39 +0200

    Add SPI_EXP support

--- src/xtp_spi.h
+++ src/xtp_spi.h
@@ -7,10 +7,24 @@
 // Create the SPI port using SPI2 on pins PB15, PB14, and PB13
 // SPIClass MySPI(SPI_MOSI_pin, SPI_MISO_pin, SPI_SCK_pin);
 
+
+#if defined(AUX_MOSI_pin) && defined(AUX_MISO_pin) && defined(AUX_SCK_pin)
+#define EXP_SPI
+SPIClass SPI_EXP;
+#endif
+
 void spi_setup() {
     SPI.setMOSI(SPI_MOSI_pin);
     SPI.setMISO(SPI_MISO_pin);
     SPI.setSCLK(SPI_SCK_pin);
+
+#ifdef EXP_SPI
+    SPI_EXP.setMOSI(AUX_MOSI_pin);
+    SPI_EXP.setMISO(AUX_MISO_pin);
+    SPI_EXP.setSCLK(AUX_SCK_pin);
+    SPI_EXP.setSSEL(AUX_CS_pin);
+    SPI_EXP.begin(AUX_CS_pin);
+#endif
 }
 
 enum SPIDeviceSelect_t {
@@ -22,7 +36,7 @@ enum SPIDeviceSelect_t {
 
 
 void spi_select(SPIDeviceSelect_t device = SPI_None) {
-    #ifdef SPI_IS_SHARED
+#ifdef SPI_IS_SHARED
     if (device != SPI_Ethernet) digitalWrite(ETH_CS_pin, HIGH);
     if (device != SPI_Flash) digitalWrite(FLASH_CS_pin, HIGH);
     if (device != SPI_Expansion) digitalWrite(AUX_CS_pin, HIGH);
@@ -32,7 +46,7 @@ void spi_select(SPIDeviceSelect_t device = SPI_None) {
         case SPI_Expansion: digitalWrite(AUX_CS_pin, LOW); break;
         default: break;
     }
-    #else // Expansion is on SPI1 while Ethernet and Flash are on SPI2
+#else // Expansion is on SPI1 while Ethernet and Flash are on SPI2
     if (device != SPI_Ethernet) digitalWrite(ETH_CS_pin, HIGH);
     if (device != SPI_Flash) digitalWrite(FLASH_CS_pin, HIGH);
     switch (device) {
@@ -40,14 +54,14 @@ void spi_select(SPIDeviceSelect_t device = SPI_None) {
         case SPI_Flash: digitalWrite(FLASH_CS_pin, LOW); break;
         default: break;
     }
-    #endif
-}
+#endif
+    }
 
 void spi_select_exp(SPIDeviceSelect_t device = SPI_None) {
-    #ifdef SPI_IS_SHARED
+#ifdef SPI_IS_SHARED
     return spi_select(device);
-    #else
+#else
     if (device != SPI_Expansion) digitalWrite(AUX_CS_pin, HIGH);
     else digitalWrite(AUX_CS_pin, LOW);
-    #endif
+#endif
 }
